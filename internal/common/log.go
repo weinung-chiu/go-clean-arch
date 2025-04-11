79a08cb512bd93fd322a01590595e54e18967610
charlie02 <charlie02_chiu@mail.chungyo.net>
2025-04-11 12:01:04 +0800

    refactor logging and trace ID handling in middleware

--- internal/common/log.go
+++ internal/common/log.go
@@ -5,8 +5,16 @@ import (
 	"log/slog"
 )
 
-// TODO: DRY
-var traceIDKey = "traceIDKey"
+type traceIDKeyType struct{} // 不導出 type，避免其他套件自建 key
+// TraceIDCtxKey 是導出的全域變數，可在整個 codebase 中作為 context key 使用
+var TraceIDCtxKey = traceIDKeyType{}
+
+func GetTraceID(ctx context.Context) string {
+	if v, ok := ctx.Value(TraceIDCtxKey).(string); ok {
+		return v
+	}
+	return ""
+}
 
 // TraceHandler wraps a slog.Handler and injects trace_id from context into logs
 type TraceHandler struct {
@@ -23,7 +31,7 @@ func (h *TraceHandler) Enabled(ctx context.Context, level slog.Level) bool {
 
 func (h *TraceHandler) Handle(ctx context.Context, r slog.Record) error {
 	// Inject trace ID if present
-	if traceID, ok := ctx.Value(traceIDKey).(string); ok {
+	if traceID, ok := ctx.Value(TraceIDCtxKey).(string); ok {
 		r.AddAttrs(slog.String("trace_id", traceID))
 	}
 	return h.handler.Handle(ctx, r)
--- internal/router/handler.go
+++ internal/router/handler.go
@@ -1,22 +1,26 @@
 package router
 
 import (
+	"context"
+	application "go-clean-arch/internal/application"
+	"go-clean-arch/internal/common"
+	"log/slog"
+	"time"
+
 	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	application "go-clean-arch/internal/application"
 )
 
-func SetupDevHandlers(router *gin.Engine, app *application.DevelopApplication) {
+func SetupDevHandlers(
+	router *gin.Engine,
+	app *application.DevelopApplication,
+	logger *slog.Logger,
+) {
 	router.Use(gin.Recovery())
 	router.Use(requestid.New())
-	router.Use(
-		requestid.New(
-			requestid.WithGenerator(func() string { return uuid.New().String() }),
-			//requestid.WithCustomHeaderStrKey("your-customer-key"),
-		),
-	)
-	//ginRouter.Use(LoggerMiddleware(ctx))
+	router.Use(TraceIDMiddleware("X-Trace-ID"))
+	router.Use(AccessLogMiddleware(logger.With("service", "DUMMY_SERVICE_NAME")))
 
 	r := router.Group("/api")
 	v1 := r.Group("/v1")
@@ -24,4 +28,51 @@ func SetupDevHandlers(router *gin.Engine, app *application.DevelopApplication) {
 	v1.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
+}
+
+// TraceIDMiddleware 從 request 中取得已有的 trace ID ，如果不存在就建立新的。取得之後會放入 Context 內供後續使用
+func TraceIDMiddleware(field string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		traceID := c.GetHeader(field)
+		if traceID == "" {
+			traceID = uuid.New().String()
+		}
+
+		ctx := context.WithValue(c.Request.Context(), common.TraceIDCtxKey, traceID)
+		c.Request = c.Request.WithContext(ctx)
+
+		c.Next()
+	}
+}
+
+// AccessLogMiddleware 實作 Access Log
+// 見 https://vgjira.atlassian.net/wiki/spaces/GTAT/pages/190809401/DataDog+GT+Log
+// TODO: 尚未完全符合格式、trace-ID, time 有 duplicate
+func AccessLogMiddleware(logger *slog.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		logger = logger.With(
+			slog.Time("timestamp", start),
+			slog.String("trace_id", common.GetTraceID(c.Request.Context())),
+		)
+		groupLogger := logger.WithGroup("log")
+		groupLogger = groupLogger.With(
+			slog.String("method", c.Request.Method),
+			slog.String("path", c.FullPath()),
+		)
+
+		c.Next()
+
+		groupLogger = groupLogger.With(
+			slog.String("user_agent", c.Request.UserAgent()),
+			slog.Int("status_code", c.Writer.Status()),
+			slog.Duration("latency", time.Since(start)),
+		)
+
+		if c.Writer.Status() >= 300 {
+			groupLogger.ErrorContext(c.Request.Context(), "request failed")
+		} else {
+			groupLogger.InfoContext(c.Request.Context(), "request complete")
+		}
+	}
 }
